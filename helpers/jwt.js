c061f0952f5d54819846d25cd77f7ec4e2b20e4e
angad <angadsharma1016@gmail.com>
2018-06-20 13:42:29 +0530

    login redirect

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = function verify(req,res,next){
     jwt.verify(req.get('Authorization'),process.env.SECRET_KEY,(err,decodedData)=>{
         if(err)
-            res.send(err);
+            res.redirect("/user/login");
         else{
             req.data = decodedData;
             next();
