3a2a209450a9f94335a1d6489818590f55465864
angad <angadsharma1016@gmail.com>
2018-07-02 19:50:16 +0530

    artists graphql

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const body_parser = require("body-parser");
 const morgan = require("morgan");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+
 require("dotenv").config();
 require("./db/connect");
 
 const app = express();
 
 app.use(morgan('dev'));
-//app.set("view engine","ejs");
-//app.use(express.static('public'));
+
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended:false}));
+
+//graphql
+app.use("/graphql",graphqlHTTP({
+    schema,
+    graphiql:true
+}));
 
 app.get("/",(req,res,next)=>{
     res.json({message:"Welcome"});
---
+++ schema/schema.js
@@ -0,0 +1,90 @@
+const Artists = require("../db/model").artists;
+const Users = require("../db/model").users;
+
+const graphql = require("graphql");
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLFloat,
+    GraphQLList,
+    GraphQLID,
+    GraphQLSchema
+} = graphql;
+
+
+
+/**
+ * @description TO hold booking data for artist
+ */
+const BookingType = new GraphQLObjectType({
+    name:"BookingType",
+    fields:()=>({
+        user:{type:GraphQLString},
+        description:{type:GraphQLString},
+        date:{type:GraphQLString},
+        time:{type:GraphQLString},
+        address:{type:GraphQLString}
+    })
+})
+
+
+
+
+/**
+ * @description To hold artist type data and query
+ */
+const ArtistType = new GraphQLObjectType({
+    name:"ArtistType",
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        passwd:{type:GraphQLString},
+        type:{type:GraphQLString},
+        location:{type:GraphQLString},
+        rate:{type:GraphQLFloat},
+        rating:{type:GraphQLFloat},
+        rated_users:{type:GraphQLInt},
+        bookings:{
+            type:new GraphQLList(BookingType),
+            resolve(parent,args){
+                let bookings = Artists.findById(parent.id);
+                return bookings.bookings;
+            }
+        }
+    })
+})
+
+
+
+
+/**
+ * @description Root queries-
+ * 
+ * 1. All Artists
+ * 
+ * 2. All Users
+ * 
+ * 3. Artists according to ID
+ * 
+ * 4. Users according to ID
+ */
+
+
+ const RootQuery = new GraphQLObjectType({
+     name:"RootQuery",
+     fields:()=>({
+         artists:{
+             type: new GraphQLList(ArtistType),
+             resolve(parent,args){
+                 return Artists.find({});
+             }
+         }
+     })
+ })
+
+
+ module.exports = new GraphQLSchema({
+    query:RootQuery
+ });
