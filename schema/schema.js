bd2f9d138b550147e166b0e4e46febf0f494c025
angad <angadsharma1016@gmail.com>
2018-07-02 20:36:20 +0530

    users gql

--- schema/schema.js
+++ schema/schema.js
@@ -60,6 +60,56 @@ const ArtistType = new GraphQLObjectType({
 
 
 /**
+ * @description
+ * 
+ * User graph
+ */
+
+ const EventType = new GraphQLObjectType({
+     name:"EventType",
+     fields:()=>({
+        name:{type:GraphQLString},
+        description:{type:GraphQLString},
+        artist:{type:GraphQLString},
+        date:{type:GraphQLString},
+        time:{type:GraphQLString},
+        address:{type:GraphQLString},
+        id:{type:GraphQLID}
+     })
+ });
+
+
+
+ const UserType = new GraphQLObjectType({
+     name:"UserType",
+     fields:()=>({
+        name:{type:GraphQLString},
+        passwd:{type:GraphQLString},
+        id:{type:GraphQLID},
+        events:{
+            type:new GraphQLList(EventType),
+            resolve(parent,args){
+                let events = Users.findById(parent.id);
+                return events.events;
+            }
+        },
+        cart:{
+            type:new GraphQLList(EventType),
+            resolve(parent,args){
+                let events = Users.findById(parent.id);
+                return events.cart;
+            }
+        }
+     })
+ })
+
+
+
+
+
+
+
+/**
  * @description Root queries-
  * 
  * 1. All Artists
@@ -79,6 +129,12 @@ const ArtistType = new GraphQLObjectType({
              type: new GraphQLList(ArtistType),
              resolve(parent,args){
                  return Artists.find({});
+             }
+         },
+         users:{
+             type: new GraphQLList(UserType),
+             resolve(parent,args){
+                 return Users.find({});
              }
          }
      })
